a37101db476728b40a55203d4fc023a8f3db40d8
vivianemenezes571 <vivianemenezes5711@gmail.com>
2025-09-26 20:34:14 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Contando até dez: ");
+Console.Write("0 ");
+Thread.Sleep(1000);
+Console.Write("1 ");
+Thread.Sleep(1000);
+Console.Write("2 ");
+Thread.Sleep(1000);
+Console.Write("3 ");
+Thread.Sleep(1000);
+Console.Write("4 ");
+Thread.Sleep(1000);
+Console.Write("5 ");
+Thread.Sleep(1000);
+Console.Write("6 ");
+Thread.Sleep(1000);
+Console.Write("7 ");
+Thread.Sleep(1000);
+Console.Write("8 ");
+Thread.Sleep(1000);
+Console.Write("9 ");
+Thread.Sleep(1000);
+Console.Write("10 ");
